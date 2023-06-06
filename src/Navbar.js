5c1ed54a5927ee2271c103e0344bcf40ad80357c
Mac <mac@Macs-MacBook-Pro-360.local>
2023-06-07 00:47:26 +0300

    Fix react router dependency errors

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,17 @@
+import React from "react";
+import {NavLink } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav>
         <label className="logo">Awesome Books</label>
         <ul>
-                <li id="display-list"><a to="/" className="menu-item active" >List</a></li>
-                <li id="display-add"><a to="/add"  className="menu-item" >Add List</a></li>
-                <li id="diplay-contact"><a to="/contact"  className="menu-item">Contact</a></li>
+                <li id="display-list">
+                    <NavLink to="/" className="menu-item active" >List</NavLink></li>
+                <li id="display-add">
+                    <NavLink to="/add"  className="menu-item" >Add List</NavLink></li>
+                <li id="diplay-contact">
+                    <NavLink to="/contact"  className="menu-item">Contact</NavLink></li>
             </ul>
     </nav>
   );
