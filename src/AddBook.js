641da90bb89971ba047d94bc28d395085a642a90
Asmae Mouradi <amouradi2002@gmail.com>
2023-06-06 14:17:52 -0700

    wrote the 3 components down

--- src/AddBook.js
+++ src/AddBook.js
@@ -1,1 +1,15 @@
-function
+function AddBook(){
+    return(
+        <>
+        <h1>Add a New Book</h1>
+        <br></br>
+        <form>
+            <input type="text" id="tile" name="title" placeholder="Title" required/><br/>
+            <input type="text" id="author" name="author" placeholder="Author" required/>
+            <input type="add" value="Add"/>
+        </form>
+        </>
+        
+    )
+}
+export default AddBook;
---
+++ src/Contact.js
@@ -0,0 +1,19 @@
+function Contact(){
+    return (
+        <>
+        <h1>Contact Information</h1>
+        <br></br>
+        <p class="free">Feel free to ask any question</p>
+        <br></br>
+        <ul>
+            <li class="phone">Phone Number:+212 694729439</li>
+            <br></br>
+            <li class="email">Email Address: amouradi2002@gmail.com</li>
+            <br></br>
+            <li class="address">1287 Fremont St, Morocco</li>
+        </ul> 
+        </>
+
+    )
+}
+export default Contact;
