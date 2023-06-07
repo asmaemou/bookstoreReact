1c629131e20d4ead2f762ab9b142d038e5830288
Asmae Mouradi <amouradi2002@gmail.com>
2023-06-06 17:56:12 -0700

    deleting the br + adding style.js and todo.js

---
+++ src/style.js
@@ -0,0 +1,36 @@
+const Container = { display: "flex", flexDirection: "column" };
+const Header = { padding: "10px 20px", textAlign: "center", color: "white" };
+const LabelContainer = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+};
+const Label = { padding: "10px 20px", textAlign: "center" };
+const FormContainer = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+};
+const ErrorMessage = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+};
+
+export const styles = {
+  Container: Container,
+  Header: Header,
+  LabelContainer: LabelContainer,
+  Label: Label,
+  ErrorMessage: ErrorMessage,
+  FormContainer: FormContainer
+}
+
+export const styles = {
+  Container: Container,
+  Header: Header,
+  LabelContainer: LabelContainer,
+  Label: Label,
+  ErrorMessage: ErrorMessage,
+  FormContainer: FormContainer
+}
